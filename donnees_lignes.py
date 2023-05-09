dc806b3c9137545ac565c96c150288bc340b8955
Lucas-brb <105973099+Lucas-brb@users.noreply.github.com>
2023-05-09 15:42:43 +0200

    plan transport 1h

--- donnees_lignes.py
+++ donnees_lignes.py
@@ -90,7 +90,7 @@ l60=[bifurcations[2],gares[43],300,110,1,2]
 
 l61=[bifurcations[4],gares[11],300,7,1,2]
 
-
+_troncons = [l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14, l15, l16, l17, l18, l19, l20, l21, l22, l22bis, l23, l24, l25, l26, l27, l27bis, l28, l28bis, l29, l30, l31, l32, l33, l34, l35, l36, l37, l38, l39, l40, l41, l42, l43, l44, l45, l46, l47, l48, l49, l50, l51, l52, l53, l54, l55, l56, l57, l58, l59, l60, l61]
 
 Ligne_Roanne_Lyon = [[l1,l2,l3,l4,l5,l6,l7,l8,l9], [all([l1[4],l2[4],l3[4],l4[4],l5[4],l6[4],l7[4],l8[4],l9[4]])]]
 Ligne_Roanne_SaintEtienne = [[l10,l11,l12], all([l10[4],l11[4],l12[4]])]
