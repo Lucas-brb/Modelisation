9fff71a9c9a1cbf614d2d39c2b90504deba5c7e1
jujudut <128389499+jujudut@users.noreply.github.com>
2023-06-07 16:21:51 +0200

    finito

--- code_grille_horaire.py
+++ code_grille_horaire.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 #dans la grille horaire, pour les gares desservies hors départ de terminus, on retrouve deux fois le nom de la gare avec deux heures correspondantes : la première quand le train arrive dans cette gare, la deuxième quand le train part de cette gare, soit 2 min après être arrivé
 
 # Données du tableau
-def Grille_horaire(ligne):
-    Gares = Liste_Gares_Heures(ligne)[0]
-    HeureDépart = Liste_Gares_Heures(ligne)[1]
+def Grille_horaire(ligne,plan):
+    Gares = Liste_Gares_Heures(ligne,plan)[0]
+    HeureDépart = Liste_Gares_Heures(ligne,plan)[1]
 # Transposer les données
-    table_data = [Gares, HeureDépart]
+    table_data = [Gares] + [HeureDépart[i] for i in range (len(HeureDépart))]
     table_data_transposed = list(map(list, zip(*table_data)))
 
 # Création du graphique
     fig, ax = plt.subplots()
 
 # Création du tableau
-    table = ax.table(cellText=table_data_transposed, colLabels=['Gares', 'Heure de passage'], loc='center')
+    table = ax.table(cellText=table_data_transposed, colLabels=['Gares', 'Heure de passage', 'Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage','Heure de passage'], loc='center')
 
 # Style du tableau
     table.auto_set_font_size(False)
@@ -26,8 +26,5 @@ def Grille_horaire(ligne):
 
 # Affichage du graphique
     plt.show()
-
-
-Liste_lignes=[Ligne_BourgEnBresse_Macon, Ligne_Chambery_Annecy, Ligne_Grenoble_Chambery, Ligne_Lyon_BourgEnBresse, Ligne_Lyon_Chambery, Ligne_Lyon_Grenoble, Ligne_Lyon_Macon, Ligne_Lyon_SaintEtienne, Ligne_Lyon_Valence, Ligne_Roanne_Lyon, Ligne_Roanne_SaintEtienne, Ligne_Valence_Grenoble,Ligne_Lyon_Annecy]
 
 
--- liste_gares_heures.py
+++ liste_gares_heures.py
@@ -1,8 +1,7 @@
-def Liste_Gares_Heures(ligne): #ligne sous la forme 'Roanne-Lyon'
+def Liste_Gares_Heures(ligne,plan): #ligne sous la forme 'Roanne-Lyon'
     troncons_l=troncons_lignes[ligne][0]
     sens_parcours=troncons_lignes[ligne][1]
     Gares=[]
-    Heures=[]
     indices=[]
 
     #création de la liste des gares desservies
@@ -38,15 +37,22 @@ def Liste_Gares_Heures(ligne): #ligne sous la forme 'Roanne-Lyon'
                 Gares.append(troncons_l[len(troncons_l)-1][0])
 
     #création de la liste des heures de passage à ces gares
-    indice_plan = plan[0].index(ligne)
-    for x in plan[2][indice_plan]:
-        Heures.append(x)
+    indice_plan = []
+    for j in range(0, len(plan[0])):
+        if plan[0][j]==ligne:
+            indice_plan.append(j)
+    Heures=[[] for _ in range (len(indice_plan))]
+    for k in range (len(indice_plan)):
+        for l in range (0, len(plan[2][indice_plan[k]])-1):
+            Heures[k].append(plan[2][indice_plan[k]][l+1])
 
     #on retire les heures correspond aux points qui ne sont pas des gares desservies
-    Heures = [Heures[i] for i in range(len(Heures)) if not(i in indices)]
+    for k in range (len(indice_plan)):
+        Heures[k] = [Heures[k][i] for i in range(len(Heures[k])) if not(i in indices)]
 
     #on met les heures sous le format '9h05'
-    for k in range (0, len(Heures)) :
-        Heures[k]=min_to_format(Heures[k])
+    for k in range  (len(indice_plan)):
+        for m in range (0, len(Heures[k])) :
+            Heures[k][m]=min_to_format(Heures[k][m])
 
     return Gares, Heures
